2aeb92bd2dc42ed02e8ffe6f2709f57a5b68dc54
Ps Peasant <StephanHC.gibson@gmail.com>
2023-09-29 13:13:35 +0200

    Change soundmanager

--- Leven-Met-Dwang/Assets/Scripts/AudioManager.cs
+++ Leven-Met-Dwang/Assets/Scripts/AudioManager.cs
@@ -2,17 +2,51 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+//This script requires a AudioSource component to work
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private AudioSource _shortAudioSource;
+    [SerializeField] private AudioSource _LongAudioSource;
+    public static AudioManager current;
+
+    //Fires after object has been initialized
+    private void Awake()
     {
-        
+        current = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    //Plays given soundfile
+    public void PlaySound(string soundName, float lowerPitch = 1.0f, float higherPitch = 1.0f)
     {
-        
+        AudioClip audioClip = Resources.Load<AudioClip>(soundName);
+        if (audioClip == null)
+        {
+            Debug.Log("Failed");
+            return;
+        }
+
+        //Adjusts pitch randomly
+        float pitchModifier = Random.Range(lowerPitch, higherPitch);
+
+        _shortAudioSource.pitch = pitchModifier;
+        _shortAudioSource.PlayOneShot(audioClip, 1);
+    }
+
+    //Play a sound that can play over a longer time (only one at a time)
+    public void PlayLongSound(string soundName)
+    {
+        AudioClip audioClip = Resources.Load<AudioClip>(soundName);
+        if (audioClip == null) return;
+
+        _LongAudioSource.clip = audioClip;
+        _LongAudioSource.Play();
+    }
+
+    //Plays one random sound out of a given array
+    public void PlayRandomSound(string[] soundNames)
+    {
+        int number = Random.Range(0, soundNames.Length);
+        PlaySound(soundNames[number]);
     }
 }
